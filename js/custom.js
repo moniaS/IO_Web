23a1009188deb816037193110b32908e13e10349
Monika <monika.stobieniecka1@wp.pl>
2016-12-11 15:39:59 +0100

    Added login, register and connection to firebase

---
+++ js/custom.js
@@ -0,0 +1,8 @@
+var config = {
+      apiKey: "AIzaSyAQCG_JCyMOVdPXWQe-djJf6tSnCZQk36c",
+      authDomain: "driving-school-5eef5.firebaseapp.com",
+      databaseURL: "https://driving-school-5eef5.firebaseio.com",
+      storageBucket: "driving-school-5eef5.appspot.com",
+      messagingSenderId: "771240715747"
+    };
+firebase.initializeApp(config);
